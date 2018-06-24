c0707ba5e05562600966c7541d4a721e044216da
MiaRene15 <kids.hines@icloud.com>
2018-06-24 16:21:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+const drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(name) {
+      drivers.push('Ralph')
+}
+
+function destructivelyPrependDriver(name) {
+      drivers.unshift('Bob')
+}
+
+function destructivelyRemoveLastDriver() {
+      drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+      drivers.shift()
+}
+
+function appendDriver(name) {
+    return drivers.concat('Broom')
+}
+
+function prependDriver(name) {
+    const drivers2 = ['Arnold', ...drivers]
+    return drivers2
+}
+
+function removeLastDriver() {
+    const drivers3 = drivers.slice(0, 2)
+    return drivers3 
+}
+
+function removeFirstDriver() {
+    const drivers3 = drivers.slice(1)
+    return drivers3 
+}
